88202fe88a7640955ea33f33ccbc2bcca315baef
bobo <atguigu0426@gg.com>
2021-08-27 23:10:05 +0800

    push commit

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -8,5 +8,6 @@ public class Test {
         System.out.println("hello shanghai");
         System.out.println("master");
         System.out.println("hot-fix");
+        System.out.println("push");
     }
 }
